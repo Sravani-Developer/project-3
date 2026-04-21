ff80ea13c5437f85f493f88a166cd206d534996a
Sravani Medagam <sravanimedagam0709@gmail.com>
2026-04-21 00:09:17 -0700

    Fix stale PWA cache behavior in production

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'eventlens-v1';
-const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png'];
+const CACHE_NAME = 'eventlens-v2';
+const APP_SHELL = ['/manifest.webmanifest', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
@@ -16,6 +16,20 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   const request = event.request;
   if (request.method !== 'GET') return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => response)
+        .catch(() => caches.match('/'))
+    );
+    return;
+  }
+
+  const requestUrl = new URL(request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
 
   event.respondWith(
     caches.match(request).then((cached) => {
